ee479d7f17c313701fdeff7c73bc7700206709b5
Luiscrdzm <luiscarlos.rodriguezm@udem.edu>
2024-04-03 12:37:43 -0600

    Eliminar funcion infuncional(:

--- functions.sql
+++ functions.sql
@@ -1,10 +1,5 @@
 DELIMITER $$
 
-CREATE OR REPLACE FUNCTION estrella(IN idP INT,IN calidad ENUM('plata','oro','iridio')) RETURNS INT
-BEGIN
-    RETURN (SELECT precioVenta*(CASE calidad WHEN 1 THEN 1.25 WHEN 2 THEN 1.5 WHEN 3 THEN 2 END) FROM plantas WHERE plantas.idP=idP);
-END $$
-
 CREATE OR REPLACE FUNCTION menorPrecio(IN id INT) RETURNS INT
 BEGIN
     RETURN (SELECT MIN(precioCompra)
